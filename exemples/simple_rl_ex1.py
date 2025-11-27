a4d4bde8ebdc90312becd0b38ab88b9da7dc8c9a
Lisandra <lisa.crisilva@outlook.com>
2025-11-27 14:57:38 -0300

    atualização dos exemplos usados

--- exemples/simple_rl_ex1.py
+++
@@ -1,9 +0,0 @@
-# Imports
-from simple_rl.run_experiments import run_agents_on_mdp
-from simple_rl.tasks import GridWorldMDP
-from simple_rl.agents import QLearningAgent
-
-# Run Experiment
-mdp = GridWorldMDP(goal_locs=[(5, 3)])
-agent = QLearningAgent(mdp.get_actions())
-run_agents_on_mdp([agent], mdp)
